0d71ac04f23d1f3f333c7b90774de5d1331ee17d
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-01-27 21:31:11 +0500

    Added Content to home

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,12 @@ import { Signika_Font } from "./layout";
 export default function Home() {
   return (
     <div>
-      <h1>hello world </h1>
+      <h1 className={`${Signika_Font.className} text-4xl my-5 mx-auto`}>
+        Welcome tot KSTP (Khyber Students Testing Portal)
+      </h1>
+      <p className="text-center my-3 p-2 text-lg">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit vitae vero repellat ad quos, illo asperiores eum aperiam temporibus sunt iusto voluptatem ipsum tempore reiciendis, nostrum dicta tenetur ab architecto neque mollitia dignissimos ut amet excepturi laboriosam. Iste atque asperiores temporibus placeat culpa dolorem delectus est officia obcaecati eveniet in a beatae iusto rerum eum magni nesciunt aliquid voluptates inventore, praesentium corrupti nostrum. Distinctio, nisi. Cupiditate dolorem, veritatis labore, asperiores tenetur eveniet natus quis, odit quo numquam explicabo ab dicta harum optio sapiente in. Quas, suscipit dolorem fugiat amet voluptas, at impedit ea dignissimos magnam in quasi delectus, eos neque aut quod. Sapiente autem quia ipsam, reiciendis possimus placeat temporibus ipsa, minus porro expedita ab delectus libero. Blanditiis nostrum assumenda voluptas vel eaque illo harum tenetur minus consectetur? Similique temporibus omnis consequatur nemo labore cum, soluta nihil ad, tenetur impedit non eligendi esse dicta, molestiae pariatur earum deserunt facilis? Nobis corrupti deleniti numquam animi ut voluptates, distinctio expedita facilis minus ipsam ratione ducimus dolor deserunt accusamus doloribus beatae corporis, quae eius soluta eaque recusandae in incidunt quisquam? Explicabo, voluptatum quia sed nemo nostrum cumque adipisci qui nulla ea dolorum eius libero maxime tempora corrupti nobis est dolorem. Saepe cumque dignissimos esse dolorum, sit vitae eveniet unde excepturi non voluptatibus, ab, deleniti quos facilis quis reprehenderit tempore perferendis expedita suscipit ea numquam laboriosam a. Dignissimos, error velit molestias qui sed veritatis perspiciatis quaerat? Eum, eius dolorum at, exercitationem, ex amet aliquid unde quas error cumque totam. Nobis, reiciendis eum. Maxime, nesciunt enim expedita dicta vitae eveniet deserunt magnam laudantium quas optio quasi quaerat repellendus consectetur temporibus quidem velit omnis numquam minima molestias, suscipit corporis delectus blanditiis. Tempore voluptatem beatae qui nisi libero a ipsam eveniet excepturi fugit. Consequuntur rerum corrupti assumenda id, ex accusamus maiores praesentium hic ratione expedita, minus voluptate.
+      </p>
     </div>
   );
 }
