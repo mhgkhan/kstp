92c8e47ac4e2c98e0d61af59b5e83964e6f0a847
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-02-03 15:44:59 +0500

    Update logo dimensions for improved display on various screen sizes

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -7,7 +7,7 @@ const Logo = () => {
             <div className="logo-area flex items-end justify-center gap-2 cursor-pointer">
 
                 <div className="logo w-[70px] h-[70px] md:w-[80px] md:h-[80px] my-1 ">
-                    <Image src={'/logo.png'} alt='KSTP LOGO' width={70} height={70} className='w-full h-full bg-white rounded-md' />
+                    <Image src={'/logo.png'} alt='KSTP LOGO' width={320} height={20} className='w-full h-full bg-white rounded-md' />
                 </div>
                 <div>
                     <p className="md:text-xl text-lg font-bold text-white underline">KHYBER STUDENTS</p>
