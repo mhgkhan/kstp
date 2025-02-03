e9a141a3a42819ae47bb9c8e1b096bb1ebce3424
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-02-03 16:54:38 +0500

    Enhance accessibility by adding roles and attributes in Page.js and Logo.jsx

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+
 const Logo = () => {
     return (
-        <Link href={`/`}>
-            <div className="logo-area flex items-end justify-center gap-2 cursor-pointer">
+        <Link href={`/`} aria-label="Home">
+            <div className="logo-area flex items-end justify-center gap-2 cursor-pointer" role="img" aria-label="KSTP Logo">
 
                 <div className="logo w-[70px] h-[70px] md:w-[80px] md:h-[80px] my-1 rounded-md">
-                    <Image src={'/logo.png'} alt='KSTP LOGO' width={320} height={20} className='w-full h-full bg-white rounded-md' />
+                    <Image src={'/logo.png'} alt='KSTP LOGO' width={80} height={80} className='w-full h-full bg-white rounded-md' />
                 </div>
                 <div>
                     <p className="md:text-xl text-lg font-bold text-white underline">KHYBER STUDENTS</p>
