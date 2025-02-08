70c144d13991a2eb69ebe90ad6759a39c290ece9
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-02-08 21:37:02 +0500

    Added Full Height to Profile Page

--- src/app/profile/layout.js
+++ src/app/profile/layout.js
@@ -5,7 +5,7 @@ import ProfileSidebar from "@/components/hide/ProfileSidebar";
 
 export default function ProfileLayout({ children }) {
     return (
-        <main className="min-h-full">
+        <main style={{height:"100vh !important"}}>
             <ProfileHeader />
             <div className="w-full h-full flex items-start justify-between gap-2l">
                 <ProfileSidebar />
