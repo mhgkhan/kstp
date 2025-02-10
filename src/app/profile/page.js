09ea7251e1df00ad602c1ebf11a7f2ed21ff3fa1
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-02-10 22:03:30 +0500

    Added some js code to short html

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { Signika_Font } from '../layout'
 import Link from 'next/link'
 import Image from 'next/image'
-import { CgKey, CgPassword, CgProfile } from "react-icons/cg"
+import { CgPassword, CgProfile } from "react-icons/cg"
 import { FaPaperPlane, FaTrophy, FaUserEdit } from 'react-icons/fa'
 import { TiTickOutline } from "react-icons/ti";
-import {AiOutlineLoading3Quarters} from "react-icons/ai"
+import { AiOutlineLoading3Quarters } from "react-icons/ai"
 import SectionHeading from '@/components/ui/SectionHeading'
 
 const page = () => {
@@ -14,7 +14,8 @@ const page = () => {
       <div className='container mx-auto flex items-center justify-around flex-wrap-reverse'>
 
         <div className='text p-2 flex items-center justify-center flex-col'>
-          <h1 className={`md:text-4xl text-2xl font-bold my-1 text-center ${Signika_Font.className}`}>Welcome Back </h1>
+          <SectionHeading heading={"Welcome Back"} paragraph={" "} />
+          {/* <h1 className={`md:text-4xl text-2xl font-bold my-1 text-center ${Signika_Font.className}`}>Welcome Back </h1> */}
           <h2 className={`md:text-3xl text-xl font-bold text-cyan-700 my-1 text-center ${Signika_Font.className}`}>Muhammad Hasnain </h2>
         </div>
 
@@ -27,25 +28,23 @@ const page = () => {
     <section className='my-2 w-full bg-white' >
       <div className='container mx-auto'>
         {/* <h1 className='md:text-4xl text-2xl font-bold my-2 p-2'>Quick Links </h1> */}
-        <SectionHeading heading={"Quick Links" } paragraph={"List of the links to access pages quickly."} />
+        <SectionHeading heading={"Quick Links"} paragraph={"List of the links to access pages quickly."} />
 
         <div className='blocks flex flex-wrap items-center justify-start gap-1'>
-          <Link href={"/profile"} className='text-xl text-white hover:bg-cyan-900 md:w-[200px] w-[100px] text-center bg-cyan-800 rounded-md shadow-md shadow-cyan-600 m-1 mx-auto p-3 flex items-center justify-center flex-col'>
-            <span className='md:text-5xl text-2xl text-cyan-400'> <CgProfile /> </span>
-            <span className="mt-1 font-bold">View profile </span>
-          </Link>
-          <Link href={"/profile"} className='text-xl text-white hover:bg-cyan-900 md:w-[200px] w-[100px] text-center bg-cyan-800 rounded-md shadow-md shadow-cyan-600 m-1 mx-auto p-3 flex items-center justify-center flex-col'>
-            <span className='md:text-5xl text-2xl text-cyan-400'> <FaUserEdit /> </span>
-            <span className="mt-1 font-bold">Edit Profile </span>
-          </Link>
-          <Link href={"/profile"} className='text-xl text-white hover:bg-cyan-900 md:w-[200px] w-[100px] text-center bg-cyan-800 rounded-md shadow-md shadow-cyan-600 m-1 mx-auto p-3 flex items-center justify-center flex-col'>
-            <span className='md:text-5xl text-2xl text-cyan-400'> <CgPassword /> </span>
-            <span className="mt-1 font-bold">Change Password </span>
-          </Link>
-          <Link href={"/profile"} className='text-xl text-white hover:bg-cyan-900 md:w-[200px] w-[100px] text-center bg-cyan-800 rounded-md shadow-md shadow-cyan-600 m-1 mx-auto p-3 flex items-center justify-center flex-col'>
-            <span className='md:text-5xl text-2xl text-cyan-400'> <FaPaperPlane /> </span>
-            <span className="mt-1 font-bold">Apply for test </span>
-          </Link>
+          {
+            Array.from([
+              { link: "/profile/userid/", icon: <CgProfile />, text: "View Profile" },
+              { link: "/profile/editinfo/", icon: <FaUserEdit />, text: "Edit Profile" },
+              { link: "/profile/changepassword/", icon: <CgPassword />, text: "Change Password" },
+              { link: "/profile/apply/", icon: <FaPaperPlane />, text: "Apply for test" },
+            ]).map((ele, ind) => {
+              return <Link key={ind} href={ele.link} className='text-xl text-white hover:bg-cyan-900 md:w-[200px] w-[100px] text-center bg-cyan-800 rounded-md shadow-md shadow-cyan-600 m-1 mx-auto p-3 flex items-center justify-center flex-col'>
+                <span className='md:text-5xl text-2xl text-cyan-400'> {ele.icon} </span>
+                <span className={`mt-1 font-bold ${Signika_Font.className}`}> {ele.text} </span>
+              </Link>
+            })
+          }
+
         </div>
       </div>
     </section>
@@ -53,20 +52,20 @@ const page = () => {
     <section className="my-2 w-full bg-white mt-4">
       <div className="container mx-auto">
         {/* <h1 className='md:text-4xl text-2xl font-bold my-2 p-2'>Analytics </h1> */}
-        <SectionHeading heading={"Analytics" } paragraph={"Your recent work and achievements"} />
+        <SectionHeading heading={"Analytics"} paragraph={"Your recent work and achievements"} />
         <div className="blocks flex flex-wrap items-center justify-start gap-1">
-        <Link href={"/profile"} className='text-xl text-cyan-900 hover:text-white bg-cyan-100 hover:bg-cyan-900 md:w-[350px] w-[95%] text-center rounded-md shadow-md shadow-cyan-600 m-1 mx-auto p-3 flex items-center justify-center flex-col'>
-            <span className='text-5xl text-cyan-500'> <AiOutlineLoading3Quarters />   10+ </span>
-            <span className="mt-1 font-bold">Tests Submitted </span>
-          </Link>
-        <Link href={"/profile"} className='text-xl text-cyan-900 hover:text-white bg-cyan-100 hover:bg-cyan-900 md:w-[350px] w-[95%] text-center rounded-md shadow-md shadow-cyan-600 m-1 mx-auto p-3 flex items-center justify-center flex-col'>
-            <span className='text-5xl text-cyan-500'> <TiTickOutline /> 10+ </span>
-            <span className="mt-1 font-bold"> Tests Passed </span>
-          </Link>
-        <Link href={"/profile"} className='text-xl text-cyan-900 hover:text-white bg-cyan-100 hover:bg-cyan-900 md:w-[350px] w-[95%] text-center rounded-md shadow-md shadow-cyan-600 m-1 mx-auto p-3 flex items-center justify-center flex-col'>
-            <span className='text-5xl text-cyan-500'> <FaTrophy /> 10+ </span>
-            <span className="mt-1 font-bold"> Positions Achieved </span>
-          </Link>
+          {
+            Array.from([
+              { link: "/profile/tests/submittedtests/", icon: <AiOutlineLoading3Quarters />, count: 20, text: "Tests Submitted" },
+              { link: "/profile/tests/passedtests/", icon: <TiTickOutline />, count: 10, text: "Tests Passed" },
+              { link: "/profile/tests/achevements/", icon: <FaTrophy />, count: 15, text: "Positions acheived" }
+            ]).map((ele, ind) => {
+              return <Link key={ind} href={ele.link} className='text-xl text-cyan-900 hover:text-white bg-cyan-100 hover:bg-cyan-900 md:w-[350px] w-[95%] text-center rounded-md shadow-md shadow-cyan-600 m-1 mx-auto p-3 flex items-center justify-center flex-col'>
+                <span className='text-5xl text-cyan-500'> {ele.icon}  {ele.count}+ </span>
+                <span className={`mt-1 font-bold ${Signika_Font.className}`}>{ele.text} </span>
+              </Link>
+            })
+          }
         </div>
       </div>
     </section>
