402f6422cb07e68c8b4a2168403a62d9191aec5e
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-02-23 20:08:55 +0500

    Add User Profile page with section heading component

---
+++ src/app/candidate/userid/page.js
@@ -0,0 +1,12 @@
+import SectionHeading from '@/components/ui/SectionHeading'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <SectionHeading heading={"User Profile"} paragraph={"View your profile details"} />
+    </div>
+  )
+}
+
+export default page
